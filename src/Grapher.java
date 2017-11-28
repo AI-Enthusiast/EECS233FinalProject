d7b5821f6c8ebdca40fff6ee3bc1e47404cedae7
Cormac <Rareear97>
2017-11-27 20:39:30 -0500

    - Added Grapher.java to graph runtimes - Altered Reporting.java so that it no longer requires an input file and outputs to a textfile. It now will push it's output to Grapher.java to be visualized.

---
+++ src/Grapher.java
@@ -0,0 +1,143 @@
+// 2-D graphing program. Written by Ian Waldschmidt for EECS 233.
+// See main method for examples of using "graph" and "display" methods.
+// Notes: (1) "graph" must be called for all data before "display".
+//        (2) import java.awt.* to use the Color class in other programs.
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineMetrics;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class Grapher extends JPanel {
+    final int PAD = 30;
+    JFrame f = new JFrame();
+    private ArrayList<int[]> xdatas = new ArrayList<int[]>();
+    private ArrayList<int[]> ydatas = new ArrayList<int[]>();
+    private ArrayList<Color> colors = new ArrayList<Color>();
+
+    public static void main(String[] args) {
+        Grapher grapher = new Grapher(); // Create single Grapher for all lines
+
+        // 1st line data
+        int[] xdata1 = {2, 4, 6, 8, 10};
+        int[] ydata1 = {100, 200, 300, 400, 500};
+        grapher.graph(xdata1, ydata1, Color.BLUE);
+
+        // 2nd line data
+        int[] xdata2 = {5, 10, 15, 20};
+        int[] ydata2 = {50, 100, 500, 1000};
+        grapher.graph(xdata2, ydata2, Color.RED);
+
+        // 3rd line data
+        int[] xdata3 = {1, 2, 3, 4, 5};
+        int[] ydata3 = {500, 550, 600, 650, 700};
+        grapher.graph(xdata3, ydata3, Color.GREEN);
+
+        grapher.display(); // Display all lines when ready
+    }
+
+    public void graph(int[] xdata, int[] ydata, Color color) {
+        if (xdata.length != ydata.length) {
+            System.out.println("Input arrays must be the same length");
+            return;
+        }
+
+        this.xdatas.add(xdata);
+        this.ydatas.add(ydata);
+        this.colors.add(color);
+    }
+
+    public void display() {
+        f.add(this);
+        f.setSize(400, 400);
+        f.setLocation(200, 200);
+        f.setVisible(true);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int w = getWidth();
+        int h = getHeight();
+        //Draw ordinate
+        g2.draw(new Line2D.Double(PAD, PAD, PAD, h - PAD));
+        //Draw abcissa
+        g2.draw(new Line2D.Double(PAD, h - PAD, w - PAD, h - PAD));
+        //Draw labels
+        Font font = g2.getFont();
+        FontRenderContext frc = g2.getFontRenderContext();
+        LineMetrics lm = font.getLineMetrics("0", frc);
+        float sh = lm.getAscent() + lm.getDescent();
+        //Write x axis values
+        double[] xaxis = new double[5];
+        xaxis[0] = 0;
+        xaxis[4] = getMax(xdatas);
+        xaxis[1] = xaxis[4] / 4;
+        xaxis[2] = xaxis[4] / 2;
+        xaxis[3] = xaxis[1] * 3;
+        int xcoord = PAD - 4;
+        int ycoord = h - (PAD / 4);
+        for (int i = 0; i < 5; i++) {
+            DecimalFormat form = new DecimalFormat("0.#");
+            String value = form.format(xaxis[i]) + "";
+            g2.drawString(value, xcoord, ycoord);
+            xcoord += (w - 2 * PAD) / 5 + font.getStringBounds(value, frc).getWidth();
+        }
+        //Write y axis values
+        double[] yaxis = new double[5];
+        yaxis[0] = 0;
+        yaxis[4] = getMax(ydatas);
+        yaxis[1] = yaxis[4] / 4;
+        yaxis[2] = yaxis[4] / 2;
+        yaxis[3] = yaxis[1] * 3;
+        xcoord = PAD / 4;
+        ycoord = h - PAD + 4;
+        for (int i = 0; i < 5; i++) {
+            DecimalFormat form = new DecimalFormat("0.#");
+            String value = form.format(yaxis[i]) + "";
+            g2.drawString(value, xcoord, ycoord);
+            ycoord -= (h - 2 * PAD) / 5 + font.getStringBounds(value, frc).getHeight();
+        }
+        //Draw data
+        double xScale = (double) (w - 2 * PAD) / getMax(xdatas);
+        double yScale = (double) (h - 2 * PAD) / getMax(ydatas);
+        for (int j = 0; j < xdatas.size(); j++) {
+            int[] xdata = this.xdatas.get(j);
+            int[] ydata = this.ydatas.get(j);
+            Color color = this.colors.get(j);
+            //Draw lines for each data set
+            g2.setPaint(color);
+            for (int i = 0; i < xdata.length - 1; i++) {
+                double x1 = PAD + xScale * xdata[i];
+                double y1 = h - PAD - yScale * ydata[i];
+                double x2 = PAD + xScale * xdata[i + 1];
+                double y2 = h - PAD - yScale * ydata[i + 1];
+                g2.draw(new Line2D.Double(x1, y1, x2, y2));
+            }
+            //Draw points for each data set
+            g2.setPaint(color);
+            for (int i = 0; i < xdata.length; i++) {
+                double x = PAD + xScale * xdata[i];
+                double y = h - PAD - yScale * ydata[i];
+                g2.fill(new Ellipse2D.Double(x - 2, y - 2, 4, 4));
+            }
+        }
+    }
+
+    private int getMax(ArrayList<int[]> data) {
+        int max = -Integer.MAX_VALUE;
+        for (int j = 0; j < data.size(); j++) {
+            for (int i = 0; i < data.get(j).length; i++) {
+                if (data.get(j)[i] > max) {
+                    max = data.get(j)[i];
+                }
+            }
+        }
+        return max;
+    }
+}
--- src/Reporting.java
+++ src/Reporting.java
@@ -1,7 +1,3 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Random;
 
 /**
@@ -96,32 +92,6 @@ public class Reporting {
     }
 
     /**
-     * Writes the table into a file
-     *
-     * @param contents contents of the file
-     */
-    private static void writeFile(String contents) {
-        BufferedWriter writer = null;
-        try {
-           /* create a temporary file */
-            File logFile = new File("READ_ME.txt");
-           /* this will output the full path where the file will be written to */
-            writer = new BufferedWriter(new FileWriter(logFile));
-            writer.write(contents);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-               /* close the writer regardless of what happens */
-                assert writer != null;
-                writer.close();
-            } catch (IOException | NullPointerException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /**
      * A method to return the average value of a given array of run times
      *
      * @param arrayOfSamples the array containing run times
@@ -130,39 +100,28 @@ public class Reporting {
     private static double meanVal(long[] arrayOfSamples) {
         double avgTime = 0;
         /* a loop to sum the run times contained in the input array */
-        for (long arrayOfSample : arrayOfSamples) {
-            avgTime = avgTime + arrayOfSample;
+        for (int i = 0; i < arrayOfSamples.length; i++){
+            avgTime += avgTime;
         }
-        return avgTime / 10;
+        return avgTime / arrayOfSamples.length;
     }
 
-    /**
-     * A method to calculate the variance for a given array of run times
-     *
-     * @param arrayOfSamples the array containing run times
-     * @return the variance
-     */
-    private static double varianceVal(long[] arrayOfSamples) {
-        double sumOfSquares = 0;
-        /* a loop to sum the squared differences between the average value and the current run time */
-        for (long arrayOfSample : arrayOfSamples) {
-            sumOfSquares = (sumOfSquares + Math.pow((arrayOfSample - meanVal(arrayOfSamples)), 2));
-        }
-        /* dividing by degrees of freedom */
-        return sumOfSquares / 9;
-    }
-
-    static long testInsertionSort(int[] arr) {
+    static long insertionSort(int[] arr) {
         Sorting.insertionSort(arr);
         return Sorting.getTime();
     }
 
-    static long testQuickSort(int[] arr) {
+    public static long bubbleSort(int[] arr){
+        Sorting.bubbleSort(arr);
+        return Sorting.getTime();
+    }
+
+    static long quickSort(int[] arr) {
         Sorting.quickSort(arr);
         return Sorting.getTime();
     }
 
-    static long testMergeSort(int[] arr) {
+    static long mergeSort(int[] arr) {
         Sorting.mergeSort(arr);
         return Sorting.getTime();
     }
@@ -217,59 +176,38 @@ public class Reporting {
 
         /* three rounds of sorting an already sorted array */
         contents = contents + "------ SORTED ------";
-        /* three rounds of checking an already sorted array */
         for (int count = 1; count < 4; count++) {
             /* insert sort */
-            arrIS1[count - 1] = testInsertionSort(arr1);
+            arrIS1[count - 1] = insertionSort(arr1);
             sortIS1 = sortIS1 + Sorting.getTime();
-            arrIS10[count - 1] = testInsertionSort(arr10);
+            arrIS10[count - 1] = insertionSort(arr10);
             sortIS10 = sortIS10 + Sorting.getTime();
-            arrIS100[count - 1] = testInsertionSort(arr100);
+            arrIS100[count - 1] = insertionSort(arr100);
             sortIS100 = sortIS100 + Sorting.getTime();
-            arrIS1000[count - 1] = testInsertionSort(arr1000);
+            arrIS1000[count - 1] = insertionSort(arr1000);
             sortIS1000 = sortIS1000 + Sorting.getTime();
 
             /* quick sort */
-            Sorting.quickSort(arr1);
-            arrQS1[count - 1] = Sorting.getTime();
+            arrQS1[count - 1] = quickSort(arr1);
             sortQS1 = sortQS1 + Sorting.getTime();
-            Sorting.quickSort(arr10);
-            arrQS10[count - 1] = Sorting.getTime();
+            arrQS10[count - 1] = quickSort(arr10);
             sortQS10 = sortQS10 + Sorting.getTime();
-            Sorting.quickSort(arr100);
-            arrQS100[count - 1] = Sorting.getTime();
+            arrQS100[count - 1] = quickSort(arr100);
             sortQS100 = sortQS100 + Sorting.getTime();
-            Sorting.quickSort(arr1000);
-            arrQS1000[count - 1] = Sorting.getTime();
+            arrQS1000[count - 1] = quickSort(arr1000);
             sortQS1000 = sortQS1000 + Sorting.getTime();
+
             /* merge sort */
-            Sorting.mergeSort(arr1);
-            arrMS1[count - 1] = Sorting.getTime();
+            arrMS1[count - 1] = mergeSort(arr1);
             sortMS1 = sortMS1 + Sorting.getTime();
-            Sorting.mergeSort(arr10);
-            arrMS10[count - 1] = Sorting.getTime();
+            arrMS10[count - 1] = mergeSort(arr10);
             sortMS10 = sortMS10 + Sorting.getTime();
-            Sorting.mergeSort(arr100);
-            arrMS100[count - 1] = Sorting.getTime();
+            arrMS100[count - 1] = mergeSort(arr100);
             sortMS100 = sortMS100 + Sorting.getTime();
-            arrMS1000[count - 1] = Sorting.getTime();
-            Sorting.mergeSort(arr1000);
+            arrMS1000[count - 1] = mergeSort(arr1000);
             sortMS1000 = sortMS1000 + Sorting.getTime();
         }
-        contents = contents + "\n\n   --- Variance Sorted ---";
-        contents = contents + "\n      Variance HS1: " + varianceVal(arrIS1);
-        contents = contents + "\n      Variance HS10: " + varianceVal(arrIS10);
-        contents = contents + "\n      Variance HS100: " + varianceVal(arrIS100);
-        contents = contents + "\n      Variance HS1000: " + varianceVal(arrIS1000);
-        contents = contents + "\n      Variance QS1: " + varianceVal(arrQS1);
-        contents = contents + "\n      Variance QS10: " + varianceVal(arrQS10);
-        contents = contents + "\n      Variance QS100: " + varianceVal(arrQS100);
-        contents = contents + "\n      Variance QS1000: " + varianceVal(arrQS1000);
-        contents = contents + "\n      Variance MS1: " + varianceVal(arrMS1);
-        contents = contents + "\n      Variance MS10: " + varianceVal(arrMS10);
-        contents = contents + "\n      Variance MS100: " + varianceVal(arrMS100);
-        contents = contents + "\n      Variance MS1000: " + varianceVal(arrMS1000);
-        contents = contents + "\n\n   --- Averages Sorted ---";
+        // --- Averages Sorted ---
         contents = contents + "\n      HeapSort1 average time(μs):" + sortIS1 / 3;
         contents = contents + "\n      HeapSort10 average time(μs):" + sortIS10 / 3;
         contents = contents + "\n      HeapSort100 average time(μs):" + sortIS100 / 3;
@@ -284,74 +222,51 @@ public class Reporting {
         contents = contents + "\n      MergeSort1000 average time(μs):" + sortMS1000 / 3;
 
 
-        /* three rounds of checking a reversed sorted array */
+        /* three rounds of sorting a reversed sorted array */
         contents = contents + "\n\n\n\n------ REVERSED ------";
         for (int count = 1; count < 4; count++) {
-            /* heap sort */
+            /* insert sort */
             reverse(arr1);
-            Sorting.insertionSort(arr1);
-            arrIS1[count - 1] = Sorting.getTime();
+            arrIS1[count - 1] = insertionSort(arr1);
             reverse(arr10);
-            Sorting.insertionSort(arr10);
-            arrIS10[count - 1] = Sorting.getTime();
+            arrIS10[count - 1] = insertionSort(arr10);
             sortIS10 = sortIS10 + Sorting.getTime();
             reverse(arr100);
-            Sorting.insertionSort(arr100);
-            arrIS100[count - 1] = Sorting.getTime();
+            arrIS100[count - 1] = insertionSort(arr100);
             sortIS100 = sortIS100 + Sorting.getTime();
             reverse(arr1000);
-            Sorting.insertionSort(arr1000);
-            arrIS1000[count - 1] = Sorting.getTime();
+            arrIS1000[count - 1] = insertionSort(arr1000);
             sortIS1000 = sortIS1000 + Sorting.getTime();
+
             /* quick sort */
             reverse(arr1);
-            Sorting.quickSort(arr1);
-            arrQS1[count - 1] = Sorting.getTime();
+            arrQS1[count - 1] = quickSort(arr1);
             sortQS1 = sortQS1 + Sorting.getTime();
             reverse(arr10);
-            Sorting.quickSort(arr10);
-            arrQS10[count - 1] = Sorting.getTime();
+            arrQS10[count - 1] = quickSort(arr10);
             sortQS10 = sortQS10 + Sorting.getTime();
             reverse(arr100);
-            Sorting.quickSort(arr100);
-            arrQS100[count - 1] = Sorting.getTime();
+            arrQS100[count - 1] = quickSort(arr100);
             sortQS100 = sortQS100 + Sorting.getTime();
             reverse(arr1000);
-            Sorting.quickSort(arr1000);
-            arrQS1000[count - 1] = Sorting.getTime();
+            arrQS1000[count - 1] = quickSort(arr1000);
             sortQS1000 = sortQS1000 + Sorting.getTime();
+
             /* merge sort */
             reverse(arr1);
-            Sorting.mergeSort(arr1);
-            arrMS1[count - 1] = Sorting.getTime();
+            arrMS1[count - 1] = mergeSort(arr1);
             sortMS1 = sortMS1 + Sorting.getTime();
             reverse(arr10);
-            Sorting.mergeSort(arr10);
-            arrMS10[count - 1] = Sorting.getTime();
+            arrMS10[count - 1] = mergeSort(arr10);
             sortMS10 = sortMS10 + Sorting.getTime();
             reverse(arr100);
-            Sorting.mergeSort(arr100);
-            arrMS100[count - 1] = Sorting.getTime();
+            arrMS100[count - 1] = mergeSort(arr100);
             sortMS100 = sortMS100 + Sorting.getTime();
             reverse(arr1000);
-            arrMS1000[count - 1] = Sorting.getTime();
-            Sorting.mergeSort(arr1000);
+            arrMS1000[count - 1] = mergeSort(arr1000);
             sortMS1000 = sortMS1000 + Sorting.getTime();
         }
-        contents = contents + "\n\n   --- Variance Reversed ---";
-        contents = contents + "\n      Variance HS1: " + varianceVal(arrIS1);
-        contents = contents + "\n      Variance HS10: " + varianceVal(arrIS10);
-        contents = contents + "\n      Variance HS100: " + varianceVal(arrIS100);
-        contents = contents + "\n      Variance HS1000: " + varianceVal(arrIS1000);
-        contents = contents + "\n      Variance QS1: " + varianceVal(arrQS1);
-        contents = contents + "\n      Variance QS10: " + varianceVal(arrQS10);
-        contents = contents + "\n      Variance QS100: " + varianceVal(arrQS100);
-        contents = contents + "\n      Variance QS1000: " + varianceVal(arrQS1000);
-        contents = contents + "\n      Variance MS1: " + varianceVal(arrMS1);
-        contents = contents + "\n      Variance MS10: " + varianceVal(arrMS10);
-        contents = contents + "\n      Variance MS100: " + varianceVal(arrMS100);
-        contents = contents + "\n      Variance MS1000: " + varianceVal(arrMS1000);
-        contents = contents + "\n\n   --- Averages Reversed ---";
+        // --- Averages Reversed ---
         contents = contents + "\n      HeapSort1 average time(μs):" + sortIS1 / 3;
         contents = contents + "\n      HeapSort10 average time(μs):" + sortIS10 / 3;
         contents = contents + "\n      HeapSort100 average time(μs):" + sortIS100 / 3;
@@ -366,74 +281,71 @@ public class Reporting {
         contents = contents + "\n      MergeSort1000 average time(μs):" + sortMS1000 / 3;
 
 
-        /* three rounds of checking a randomly sorted array */
+        /* three rounds of sorting a randomly sorted array */
         contents = contents + "\n\n\n\n------ RANDOM ------";
         for (int count = 1; count < 4; count++) {
-               /* heap sort */
+            /* insert sort */
             shuffle(arr1);
-            Sorting.insertionSort(arr1);
-            arrIS1[count - 1] = Sorting.getTime();
+            arrIS1[count - 1] = insertionSort(arr1);
+            sortIS1 = sortIS1 + Sorting.getTime();
             shuffle(arr10);
-            Sorting.insertionSort(arr10);
-            arrIS10[count - 1] = Sorting.getTime();
+
+            arrIS10[count - 1] = insertionSort(arr10);
             sortIS10 = sortIS10 + Sorting.getTime();
             shuffle(arr100);
+
             Sorting.insertionSort(arr100);
-            arrIS100[count - 1] = Sorting.getTime();
+            arrIS100[count - 1] = insertionSort(arr100);
             sortIS100 = sortIS100 + Sorting.getTime();
             shuffle(arr1000);
+
             Sorting.insertionSort(arr1000);
-            arrIS1000[count - 1] = Sorting.getTime();
+            arrIS1000[count - 1] = insertionSort(arr1000);
             sortIS1000 = sortIS1000 + Sorting.getTime();
+
             /* quick sort */
             shuffle(arr1);
             Sorting.quickSort(arr1);
             arrQS1[count - 1] = Sorting.getTime();
             sortQS1 = sortQS1 + Sorting.getTime();
             shuffle(arr10);
+
             Sorting.quickSort(arr10);
             arrQS10[count - 1] = Sorting.getTime();
             sortQS10 = sortQS10 + Sorting.getTime();
             shuffle(arr100);
+
             Sorting.quickSort(arr100);
             arrQS100[count - 1] = Sorting.getTime();
             sortQS100 = sortQS100 + Sorting.getTime();
             shuffle(arr1000);
+
             Sorting.quickSort(arr1000);
             arrQS1000[count - 1] = Sorting.getTime();
             sortQS1000 = sortQS1000 + Sorting.getTime();
+
             /* merge sort */
             shuffle(arr1);
             Sorting.mergeSort(arr1);
             arrMS1[count - 1] = Sorting.getTime();
             sortMS1 = sortMS1 + Sorting.getTime();
             shuffle(arr10);
+
             Sorting.mergeSort(arr10);
             arrMS10[count - 1] = Sorting.getTime();
             sortMS10 = sortMS10 + Sorting.getTime();
             shuffle(arr100);
+
             Sorting.mergeSort(arr100);
             arrMS100[count - 1] = Sorting.getTime();
             sortMS100 = sortMS100 + Sorting.getTime();
             shuffle(arr1000);
-            arrMS1000[count - 1] = Sorting.getTime();
+
             Sorting.mergeSort(arr1000);
+            arrMS1000[count - 1] = Sorting.getTime();
             sortMS1000 = sortMS1000 + Sorting.getTime();
         }
-        contents = contents + "\n\n   --- Variance Random ---";
-        contents = contents + "\n      Variance HS1: " + varianceVal(arrIS1);
-        contents = contents + "\n      Variance HS10: " + varianceVal(arrIS10);
-        contents = contents + "\n      Variance HS100: " + varianceVal(arrIS100);
-        contents = contents + "\n      Variance HS1000: " + varianceVal(arrIS1000);
-        contents = contents + "\n      Variance QS1: " + varianceVal(arrQS1);
-        contents = contents + "\n      Variance QS10: " + varianceVal(arrQS10);
-        contents = contents + "\n      Variance QS100: " + varianceVal(arrQS100);
-        contents = contents + "\n      Variance QS1000: " + varianceVal(arrQS1000);
-        contents = contents + "\n      Variance MS1: " + varianceVal(arrMS1);
-        contents = contents + "\n      Variance MS10: " + varianceVal(arrMS10);
-        contents = contents + "\n      Variance MS100: " + varianceVal(arrMS100);
-        contents = contents + "\n      Variance MS1000: " + varianceVal(arrMS1000);
-        contents = contents + "\n\n   --- Averages Random ---";
+        //  --- Averages Random ---
         contents = contents + "\n      HeapSort1 average time(μs):" + sortIS1 / 3;
         contents = contents + "\n      HeapSort10 average time(μs):" + sortIS10 / 3;
         contents = contents + "\n      HeapSort100 average time(μs):" + sortIS100 / 3;
@@ -447,7 +359,6 @@ public class Reporting {
         contents = contents + "\n      MergeSort100 average time(μs):" + sortMS100 / 3;
         contents = contents + "\n      MergeSort1000 average time(μs):" + sortMS1000 / 3;
 
-        /* write dat file */
-        writeFile(contents);
+
     }
 }
